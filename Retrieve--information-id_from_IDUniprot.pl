0f4f54d8ce64884499088e47483240bcc8754702
bioinfproject <eduardoz@xanum.uam.mx>
2018-09-02 06:04:58 -0500

    uniprot information retrieve

---
+++ Retrieve--information-id_from_IDUniprot.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+
+######################################################################
+##### script of UNIPROT for download fasta file from ID Uniprot ######
+######################################################################
+
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+
+my $list = $ARGV[0]; # File containg list of UniProt identifiers.
+
+my $base = 'http://www.uniprot.org';
+my $tool = 'uploadlists';
+
+my $contact = ''; # Please set your email address here to help us debug in case of problems.
+my $agent = LWP::UserAgent->new(agent => "libwww-perl $contact");
+push @{$agent->requests_redirectable}, 'POST';
+
+my $response = $agent->post("$base/$tool/",
+                            [ 'file' => [$list],
+                              'format' => 'tab',
+                              'from' => 'ACC',
+                              'to' => 'ACC',
+						'columns' => 'protein names,genes,organism,organism-id,domain',
+                            ],'Content_Type' => 'form-data');
+
+while (my $wait = $response->header('Retry-After')) {
+  print STDERR "Waiting ($wait)...\n";
+  sleep $wait;
+  $response = $agent->get($response->base);
+}
+
+$response->is_success ?
+  print $response->content :
+  die 'Failed, got ' . $response->status_line .
+    ' for ' . $response->request->uri . "\n";
